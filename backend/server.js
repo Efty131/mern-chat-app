3589c563c7fe27d289931953ec7ac2ee6f5bffe5
Efty <efty131331@gmail.com>
2022-05-01 09:37:09 +0600

    initial commit

---
+++ backend/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const dotenv = require("dotenv");
+const { chats } = require("./data/data");
+const connectDB = require("./config/db");
+const app = express();
+const colors = require("colors");
+const userRoutes = require("./routes/userRoutes");
+const { notFound, errorHandler } = require('./middlewares/errorMiddleware');
+dotenv.config();
+connectDB()
+const PORT = process.env.PORT || 5000;
+
+app.use(express.json()); // to accept json data
+
+app.get("/", (req,res) =>{
+    res.send("API is running successfully");
+});
+
+
+app.use("/api/user", userRoutes);
+
+app.use(notFound);
+app.use(errorHandler);
+
+
+app.listen(PORT, console.log(`Server started on PORT ${PORT}`.blue.bold));
